3404b57c3ad4a8a4d99e23a13f2e45e9d4a427bd
danjobo <danieljboynton@gmail.com>
2023-05-02 18:16:07 -0700

    5-2 Update

---
+++ test-folder/additional_classes.py
@@ -0,0 +1,5 @@
+class RenderObject:
+    'represents a rendered object on the screen'
+
+    def __init__(self, image, x, y):
+        pass
--- test-folder/pygame-testing.py
+++ test-folder/pygame-testing.py
@@ -3,6 +3,7 @@
 
 import sys
 import pygame as pg
+from additional_classes import *
 from pygame.locals import *
 pg.init()
 
@@ -15,17 +16,25 @@ SCREEN_HEIGHT = 720
 screen = pg.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT),pg.RESIZABLE)
 pg.display.set_caption("Inventory Trainer")
 
-screen.fill((255,255,255))
+clock = pg.time.Clock()
 
-run = True
-while run:
-    
-    pg.display.update()
+
+inventoryBG = pg.image.load('./assets/misc/inventory-snip.png')
+inventoryRect = inventoryBG.get_rect()
+invX, invY = 287, 27
+
+
+
+running = True
+while running:
+    mouseX, mouseY = pg.mouse.get_pos()
+
+    screen.fill((255,255,255))
 
     for event in pg.event.get():
         # Close window button
         if event.type == QUIT:
-            run = False
+            running = False
         # Mouse hold/click
         if event.type == MOUSEBUTTONDOWN:
             pos=pg.mouse.get_pos()
@@ -37,11 +46,32 @@ while run:
             print ("x = {}, y = {}".format(pos[0], pos[1]))
         # Key Press
         if event.type == KEYDOWN:
+            if event.key == K_RIGHT:
+                invX += 5
+            if event.key == K_LEFT:
+                invX -= 5
+            if event.key == K_UP:
+                invY -= 5
+            if event.key == K_DOWN:
+                invY += 5
             key = pg.key.name(event.key)
             print(key, 'is pressed')
         if event.type == KEYUP:
             key = pg.key.name(event.key)
             print(key, 'is released')
 
+
+    # RENDER GAME HERE
+    inventoryRect.topleft = invX, invY
+    screen.blit(inventoryBG, inventoryRect)
+
+
+    # Makes it work on your screen
+    pg.display.flip()
+    
+    # Sets FPS to 60
+    clock.tick(60)
+
+pg.display.update()
 pg.quit()
 sys.exit(1)
